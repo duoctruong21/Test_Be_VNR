f576e5fa15dc911075b9eaf2f4efceb07dad45ce
duoctruong21 <112966857+duoctruong21@users.noreply.github.com>
2023-03-01 15:34:10 +0700

    Add project files.

---
+++ Test_Be_VNR/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Test_Be_VNR
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Test_Be_VNR/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test_Be_VNR.Models;
+
+namespace Test_Be_VNR.Controllers
+{
+    public class HomeController : Controller
+    {
+        VNR_InternShipEntities1 db = new VNR_InternShipEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MonHoc(int id) {
+            var monhoc = db.MonHocs.Find(id);
+            var khoahoc = db.KhoaHocs.Find(id);
+            ViewBag.KhocHoc = khoahoc.TenKhoaHoc;
+            if (monhoc != null)
+            {
+                var monhocs = db.MonHocs.Where(x=>x.KhoaHocID == id);                
+                return View(monhocs);
+            }
+            return View(monhoc);
+        }
+
+        public ActionResult KhoaHoc()
+        {
+            var khoahoc = db.KhoaHocs.ToList();
+            
+            return View(khoahoc);
+        }
+    }
+}
